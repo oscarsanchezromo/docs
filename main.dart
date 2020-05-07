3644c265b896d97aa8cde7549bad9f3253544bd0
oscarsanchezromo <51491251+oscarsanchezromo@users.noreply.github.com>
2020-05-06 19:44:48 -0500

    Add files via upload
    Terminado

---
+++ main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Acerca de.....',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:PaginaInicio( ),
+    );
+  }
+}
+class PaginaInicio extends StatefulWidget {
+  _PaginaInicio createState() => new _PaginaInicio();
+  
+}
+class _PaginaInicio extends State<PaginaInicio>{
+ @override
+ Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/img/flutter.png."),
+            Text("Hilda Hernandez Guerrero 1718110975 Ti 41" , style: TextStyle
+              (color: Colors.indigo),),
+            RaisedButton(
+              child: Text("apps")
+                shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                  bottomLeft:Radius.circular(15),
+                  bottomRight:Radius.circular(15),
+                )
+              ),
+              onPressed: (){
+
+              }
+            )
+          ]
+        )
+      )
+    );
+  } 
+}
